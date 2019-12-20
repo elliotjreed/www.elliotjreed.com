4d441034f43bbf5b871c0d94146b3821fa58fdea
Elliot J. Reed <git@elliotjreed.com>
2019-12-20 11:48:19 +0000

    Updates

---
+++ precache-manifest.2aff3886b85f8f8381cc679e7c67be3b.js
@@ -0,0 +1,75 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2497bc729b838aa343a94482cecb129e",
+    "url": "/./index.html"
+  },
+  {
+    "revision": "53cea1c7a0eb13af37fe44db255689b8",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "513cebe8eaa528c1b689c98b6522ee70",
+    "url": "/humans.txt"
+  },
+  {
+    "url": "/icon_128x128.fc1ca0931f43dfd037d4c4075dfcc8c1.png"
+  },
+  {
+    "url": "/icon_150x150.da88a675702e9cb7505d9830ccb6a082.png"
+  },
+  {
+    "url": "/icon_192x192.263fece7ae49c818a1d470642f113f0e.png"
+  },
+  {
+    "url": "/icon_256x256.2bed42310189c728bb74ea955a214b77.png"
+  },
+  {
+    "url": "/icon_384x384.d852fbabe9182abefb6d5a06b9f45cd1.png"
+  },
+  {
+    "url": "/icon_512x512.9393574c4f66756d55dd2b698d21191e.png"
+  },
+  {
+    "url": "/icon_96x96.302341f019323b13ec2531207e46fd17.png"
+  },
+  {
+    "url": "/js/cv.83335c67b7687537b992.min.js"
+  },
+  {
+    "url": "/js/home.83335c67b7687537b992.min.js"
+  },
+  {
+    "url": "/js/main.83335c67b7687537b992.min.js"
+  },
+  {
+    "url": "/js/pagenotfound.83335c67b7687537b992.min.js"
+  },
+  {
+    "url": "/js/post.83335c67b7687537b992.min.js"
+  },
+  {
+    "url": "/js/posts.83335c67b7687537b992.min.js"
+  },
+  {
+    "url": "/js/sitemap.83335c67b7687537b992.min.js"
+  },
+  {
+    "url": "/js/travelling.83335c67b7687537b992.min.js"
+  },
+  {
+    "url": "/js/vendor.83335c67b7687537b992.min.js"
+  },
+  {
+    "url": "/js/vendor.83335c67b7687537b992.min.js.LICENSE"
+  },
+  {
+    "url": "/main.83335c67b7687537b992.css"
+  },
+  {
+    "url": "/manifest.ac4a0b09c3e9e04421cc04df78f849e5.json"
+  },
+  {
+    "revision": "f4e54976637677c759f0ff4cf28a8c6f",
+    "url": "/robots.txt"
+  }
+]);
