3691c95ff79ffb6036f7be7e77489827eea60668
Elliot J. Reed <git@elliotjreed.com>
2019-12-20 11:36:04 +0000

    Adds gh-pages branch

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM nginx:stable-alpine
-
-WORKDIR /var/www/html
-
-COPY ./nginx.conf /etc/nginx/nginx.conf
-COPY ./dist /var/www/html
-
-EXPOSE 81
--- src/components/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export const Spinner = (): JSX.Element => {
-  return (
-    <svg className="spinner" viewBox="0 0 50 50">
-      <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5" />
-    </svg>
-  );
-};
