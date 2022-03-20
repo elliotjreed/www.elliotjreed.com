d3a463f90250af0ed845e9ada46bb4f94d41927d
Elliot J. Reed <git@elliotjreed.com>
2022-03-20 11:46:52 +0000

    Adds CORS and CSP

--- src/server/express.tsx
+++ src/server/express.tsx
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import fs from "fs";
 import ReactDOMServer from "react-dom/server";
@@ -18,6 +19,12 @@ const redirectNonWwwTraffic = (req, res, next) => {
 
 server.use(redirectNonWwwTraffic);
 
+server.use(
+  cors({
+    origin: "https://www.elliotjreed.com"
+  })
+);
+
 const oneYear = 365 * 24 * 60 * 60;
 server.use("/", express.static(join(__dirname, "static"), { index: false, maxAge: oneYear }));
 
@@ -26,6 +33,11 @@ const indexHTML: string = fs.readFileSync(resolve(__dirname, "./static/index.htm
 });
 
 server.get("*", (req, res) => {
+  res.set({
+    "Content-Security-Policy":
+      "default-src 'self' https://api.elliotjreed.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://ssl.google-analytics.com; img-src 'self' data: https://res.cloudinary.com https://www.google-analytics.com https://ssl.google-analytics.com https://csi.gstatic.com https://www.google.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://themes.googleusercontent.com https://fonts.gstatic.com; frame-src https://www.google.com; object-src 'none'"
+  });
+
   const component = ReactDOMServer.renderToString(
     <StaticRouter location={req.url}>
       <App />
