8096f8f84dd6464507654da2159d54817ce49982
Elliot J. Reed <git@elliotjreed.com>
2022-05-03 20:44:04 +0100

    Adds Google Analytics linker (for AMP site)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { hydrateRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { initialize } from "react-ga";
+import { ga, initialize } from "react-ga";
 
 import "./polyfills";
 import { App } from "./components/App";
@@ -12,7 +12,12 @@ if ("serviceWorker" in navigator) {
   });
 }
 
-initialize("UA-90440102-1");
+initialize("UA-90440102-1", { gaOptions: { allowLinker: true } });
+ga({
+  linker: {
+    domains: ["www.elliotjreed.com", "amp.elliotjreed.com"]
+  }
+});
 
 const rootEl: Element = document.getElementById("root");
 
