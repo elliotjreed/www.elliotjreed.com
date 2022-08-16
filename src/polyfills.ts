c5ac3c58b4cdfc7c3f62b1759673e7de5a8cd853
Elliot J. Reed <git@elliotjreed.com>
2022-08-16 11:27:47 +0100

    Updates dependencies

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -3,4 +3,3 @@ import "core-js/modules/es.object.entries";
 import "core-js/modules/es.object.values";
 import "core-js/modules/es.string.pad-end";
 import "core-js/modules/es.string.pad-start";
-import "abortcontroller-polyfill/dist/abortcontroller-polyfill-only";
