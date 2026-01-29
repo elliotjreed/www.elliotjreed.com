9731f381b98ffa132db3cb356ff2c05e143c9592
Elliot J. Reed <git@elliotjreed.com>
2026-01-29 22:57:04 +0000

    Excludes XML sitemap from SW

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,8 @@ export default defineConfig(({ isSsrBuild }) => ({
       injectRegister: "auto",
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+        navigateFallback: "index.html",
+        navigateFallbackDenylist: [/^\/sitemap\.xml$/],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
